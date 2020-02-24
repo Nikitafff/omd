9b13c914f1a86596bbf1ffd88a02d4926d49bac8
Nikitafff <44053679+Nikitafff@users.noreply.github.com>
2020-02-24 11:00:39 +0300

    game logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,13 +1,30 @@
+from itertools import chain
 class Board:
     def __init__(self, size):
         self.size = size
         self.desk = {}
+        self.cols = None
     
     def init_board(self) -> None:
         for column in range(self.size):
-            self.desk.update([(chr(65+column).lower() + str(i + 1), "") for i in range(self.size)])
+            self.desk.update([(chr(65+column).lower() + str(i + 1), ".") for i in range(self.size)])
+        self.cols = list(dict.fromkeys([list(self.desk.keys())[x][0] for x, y in enumerate(self.desk.keys())]))
+
+    def print_board(self):
+        row = list(map(str, list(range(1, len(self.cols)+1))))
+        for key, value in self.desk.items():
+            print(int(key[-1]))
+            row.insert(int(key[-1]), value)
+
+        #row.insert()
+        print(row)
+        board = "  %s\n%s" % ("   ".join(self.cols), "\n".join(row))
+        print(board)
+
+
+
 
 
 board = Board(3)
 board.init_board()
-print(board.desk)
+board.print_board()
